98fbcfd0ede5b86ea918016996cf7780a40d8983
NZheng_Home <nathanzheng87@gmail.com>
2018-09-09 22:27:53 -0700

    JavaFX tutorial up to Lesson 9 - grid pane

---
+++ FXBasics/src/EmbeddingLayouts.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class EmbeddingLayouts extends Application {
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("window title");
+
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300, 250);
+
+        // can't stick leftMenu in here, because it's already in a scene graph and cannot be set as root.
+//        Scene scene = new Scene(leftMenu, 300, 250);
+
+        window.setScene(scene);
+        window.show();
+    }
+}
---
+++ FXBasics/src/GridPaneMain.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GridPaneMain extends Application {
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("my grid pane tutorial");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));    // outside padding
+        grid.setVgap(8);                                // between each cell
+        grid.setHgap(10);
+
+        Label nameLabel = new Label("User name:");
+        GridPane.setConstraints(nameLabel, 0, 0);
+
+        TextField nameInput = new TextField("nzheng");
+        GridPane.setConstraints(nameInput, 1, 0);
+
+        Label pwLabel = new Label("Password:");
+        GridPane.setConstraints(pwLabel, 0, 1);
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("password");    // the gray text as "hint"
+        GridPane.setConstraints(passwordField, 1, 1);
+
+        Button loginButton = new Button("Login");
+        GridPane.setConstraints(loginButton, 1, 2);
+
+        grid.getChildren().addAll(nameLabel, nameInput, pwLabel, passwordField, loginButton);
+
+        Scene scene = new Scene(grid, 300, 200);
+        window.setScene(scene);
+
+        window.show();
+    }
+}
