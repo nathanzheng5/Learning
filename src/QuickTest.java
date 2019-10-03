68c425ec1f729bc755bf9236859352f714cb3bb6
nzheng <nathan.zheng@kardium.com>
2019-10-02 17:02:52 -0700

    trivial

--- src/QuickTest.java
+++ src/QuickTest.java
@@ -2,9 +2,7 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.text.ParsePosition;
+import java.text.*;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalField;
@@ -16,34 +14,8 @@ import java.util.stream.Collectors;
 public class QuickTest {
 
     public static void main(String[] args) throws ParseException {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        frame.setSize(800, 600);
-
-        JScrollPane scrollPane = new JScrollPane();
-        panel.add(scrollPane);
-
-        JPanel child = new JPanel();
-        child.setBorder(BorderFactory.createLineBorder(Color.RED));
-        child.setLayout(new BoxLayout(child, BoxLayout.PAGE_AXIS));
-        child.add(new JLabel("hello"));
-        child.add(new JLabel("world"));
-        JPanel child2 = new JPanel();
-        child2.setBorder(BorderFactory.createLineBorder(Color.RED));
-        child2.setLayout(new BoxLayout(child2, BoxLayout.PAGE_AXIS));
-        child2.add(new JLabel("hello"));
-        child2.add(new JLabel("world2"));
-//        scrollPane.add(child);
-
-        frame.setContentPane(panel);
-        panel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        scrollPane.getViewport().add(child);
-        scrollPane.getViewport().add(child2);
-
-//        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        System.out.println(MessageFormat.format("{0,number,#.#}", 1.0));
+        System.out.println(new DecimalFormat("#.###").format(-0.5123));
 
     }
 
