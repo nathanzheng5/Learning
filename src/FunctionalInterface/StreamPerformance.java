e6f8edc2a2472fd6b82e73dd509e4e24ad0c806f
nathanzheng5 <nathanzheng87@gmail.com>
2019-12-10 16:42:34 -0800

    StreamPerformance update.

--- src/FunctionalInterface/StreamPerformance.java
+++ src/FunctionalInterface/StreamPerformance.java
@@ -8,13 +8,15 @@ import java.util.stream.Collectors;
 public class StreamPerformance {
 
     public static void main(String[] args) {
+        System.out.println("initializing source list");
         List<Double> sourceList = new ArrayList<>();
         for (int i = 0; i < 1e7; i++) {
             final Random random = new Random();
-            boolean filteredElement = random.nextBoolean();
-            sourceList.add(filteredElement ? 0.0 : random.nextDouble());
+            boolean addZero = random.nextBoolean();
+            sourceList.add(addZero ? 0.0 : random.nextDouble());
         }
 
+        System.out.println("\nstarting 1");
         List<Double> filteredList = new ArrayList<>();
         final long startTime = System.currentTimeMillis();
         for (double element : sourceList) {
@@ -25,6 +27,7 @@ public class StreamPerformance {
         System.out.println("for loop takes " + (System.currentTimeMillis() - startTime)
                 + ", end list size = " + filteredList.size());
 
+        System.out.println("\nstarting 2");
         List<Double> filteredList2 = new ArrayList<>();
         final long startTime2 = System.currentTimeMillis();
         filteredList2.addAll(
@@ -34,6 +37,7 @@ public class StreamPerformance {
         System.out.println("stream addAll takes " + (System.currentTimeMillis() - startTime2)
                 + ", end list size = " + filteredList2.size());
 
+        System.out.println("\nstarting 3");
         final long startTime3 = System.currentTimeMillis();
         final List<Double> filteredList3 = sourceList.stream()
                 .filter(element -> element != 0.0)
@@ -41,6 +45,7 @@ public class StreamPerformance {
         System.out.println("stream collect takes " + (System.currentTimeMillis() - startTime3)
                 + ", end list size = " + filteredList3.size());
 
+        System.out.println("\nstarting 4");
         List<Double> filteredList4 = new ArrayList<>();
         final long startTime4 = System.currentTimeMillis();
         filteredList4.addAll(
@@ -50,6 +55,7 @@ public class StreamPerformance {
         System.out.println("parallel stream add takes " + (System.currentTimeMillis() - startTime4)
                 + ", end list size = " + filteredList4.size());
 
+        System.out.println("\nstarting 5");
         final long startTime5 = System.currentTimeMillis();
         List<Double> filteredList5 = sourceList.parallelStream()
                 .filter(element -> element != 0.0)
